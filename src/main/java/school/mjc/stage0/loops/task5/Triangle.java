b198bfb1e5913411c3ae91d260f8721e5d38ea91
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-14 18:19:56 +0600

    Number #1

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int i = cathetusLength - 1; i >= 0; i--){
+            for (int j = i; j < cathetusLength; j++){
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
     }
 }
